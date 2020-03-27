8bea74d6770c46f109ffe38d231a17dc5c8bc952
Thuong Le <thuong.le@nfq.asia>
2020-03-27 21:16:54 +0700

    Rename invalidated_at to redeemed_at

--- src/Migration/Migration1585287205SoldVoucher.php
+++ src/Migration/Migration1585287205SoldVoucher.php
@@ -16,19 +16,19 @@ class Migration1585287205SoldVoucher extends MigrationStep
     {
         $connection->executeQuery('CREATE TABLE IF NOT EXISTS `sold_voucher` (
     `id` BINARY(16) NOT NULL,
-    `merchat_id` BINARY(16) NOT NULL,
+    `merchant_id` BINARY(16) NOT NULL,
     `order_line_item_id` BINARY(16) NOT NULL,
     `code` CHAR(10) NOT NULL,
     `name` VARCHAR (255) NOT NULL,
-    `redeem_at` DATETIME(3) NULL,
+    `redeemed_at` DATETIME(3) NULL,
     `created_at` DATETIME(3) NOT NULL,
     `updated_at` DATETIME(3) NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `sold_voucher` (`merchant_id`,`code`)
+    UNIQUE KEY `sold_voucher` (`merchant_id`,`code`),
     KEY `fk.sold_voucher.order_line_item_id` (`order_line_item_id`),
     KEY `fk.sold_voucher.merchant_id` (`merchant_id`),
-    CONSTRAINT `fk.sold_voucher.order_line_item_id` FOREIGN KEY (`order_line_item_id`) REFERENCES `order_line_item` (`id`)
-    CONSTRAINT `fk.sold_voucher.merchant_id` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`id`),
+    CONSTRAINT `fk.sold_voucher.order_line_item_id` FOREIGN KEY (`order_line_item_id`) REFERENCES `order_line_item` (`id`),
+    CONSTRAINT `fk.sold_voucher.merchant_id` FOREIGN KEY (`merchant_id`) REFERENCES `merchant` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;');
     }
 
