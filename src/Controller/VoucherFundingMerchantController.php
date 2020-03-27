62df10236454bb4fd8cdd40b368d43af8b71ab29
Mac Hung <h.mac@shopware.com>
2020-03-28 03:56:29 +0700

    implement render pdf and send mail

--- src/Controller/VoucherFundingMerchantController.php
+++ src/Controller/VoucherFundingMerchantController.php
@@ -3,8 +3,14 @@
 namespace SwagVoucherFunding\Controller;
 
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Production\Merchants\Content\Merchant\SalesChannelContextExtension;
 use Shopware\Storefront\Controller\StorefrontController;
 use SwagVoucherFunding\Service\VoucherFundingMerchantService;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Shopware\Core\Checkout\Cart\Exception\CustomerNotLoggedInException;
 
 /**
  * @RouteScope(scopes={"storefront"})
@@ -17,4 +23,18 @@ class VoucherFundingMerchantController extends StorefrontController
     {
         $this->voucherFundingService = $voucherFundingService;
     }
+
+    /**
+     * @Route("/merchant-api/{version}/voucher/redeem", name="merchant-api.action.voucher-redeem", methods={"POST"})
+     * @throws CustomerNotLoggedInException
+     */
+    public function redeemVoucher(Request $request, SalesChannelContext $context): JsonResponse
+    {
+        $merchant = SalesChannelContextExtension::extract($context);
+
+        $this->voucherFundingService->redeemVoucher($merchant, $context);
+
+        return new JsonResponse(['code' => 200, 'Content' => 'success']);
+    }
+
 }
