a32709eed222596e758c1d5a6920c2e1675731d6
Soner Sayakci <s.sayakci@shopware.com>
2020-04-03 08:19:11 +0200

    Apply changes for new merchant-api

--- src/Controller/VoucherFundingMerchantController.php
+++ src/Controller/VoucherFundingMerchantController.php
@@ -5,7 +5,7 @@ namespace SwagVoucherFunding\Controller;
 use Shopware\Core\Checkout\Cart\Exception\CustomerNotLoggedInException;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
-use Shopware\Production\Merchants\Content\Merchant\SalesChannelContextExtension;
+use Shopware\Production\Merchants\Content\Merchant\MerchantEntity;
 use Shopware\Storefront\Controller\StorefrontController;
 use SwagVoucherFunding\Service\VoucherFundingMerchantService;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @RouteScope(scopes={"storefront"})
+ * @RouteScope(scopes={"merchant-api"})
  */
 class VoucherFundingMerchantController extends StorefrontController
 {
@@ -28,10 +28,8 @@ class VoucherFundingMerchantController extends StorefrontController
      * @Route(name="merchant-api.action.sold.vouchers.load", path="/merchant-api/v{version}/voucher-funding/sold/vouchers", methods={"GET"})
      * @throws CustomerNotLoggedInException
      */
-    public function loadSoldVouchers(SalesChannelContext $context) : JsonResponse
+    public function loadSoldVouchers(MerchantEntity $merchant, SalesChannelContext $context) : JsonResponse
     {
-        $merchant = SalesChannelContextExtension::extract($context);
-
         $soldVouchers = $this->voucherFundingService->loadSoldVouchers($merchant->getId(), $context);
 
         return new JsonResponse([
@@ -40,18 +38,16 @@ class VoucherFundingMerchantController extends StorefrontController
     }
 
     /**
-     * @Route("/merchant-api/v{version}/voucher-funding/voucher/redeem", name="merchant-api.action.voucher-funding.voucher.redeem", methods={"POST"}, defaults={"csrf_protected"=false})
+     * @Route("/merchant-api/v{version}/voucher-funding/voucher/redeem", name="merchant-api.action.voucher-funding.voucher.redeem", methods={"POST"})
      * @throws CustomerNotLoggedInException
      */
-    public function redeemVoucher(Request $request, SalesChannelContext $context): JsonResponse
+    public function redeemVoucher(Request $request, MerchantEntity $merchant, SalesChannelContext $context): JsonResponse
     {
         $voucherCode = $request->request->get('code');
 
         if (!$voucherCode) {
             throw new \InvalidArgumentException('Please input voucher code to redeem');
         }
-
-        $merchant = SalesChannelContextExtension::extract($context);
 
         $this->voucherFundingService->redeemVoucher($voucherCode, $merchant, $context);
 
@@ -62,7 +58,7 @@ class VoucherFundingMerchantController extends StorefrontController
      * @Route("/merchant-api/v{version}/voucher-funding/voucher/status", name="merchant-api.action.voucher-funding.voucher.status", methods={"GET"})
      * @throws CustomerNotLoggedInException
      */
-    public function getVoucherStatus(Request $request, SalesChannelContext $context): JsonResponse
+    public function getVoucherStatus(Request $request, MerchantEntity $merchant, SalesChannelContext $context): JsonResponse
     {
         $voucherCode = $request->query->get('code');
 
@@ -70,11 +66,9 @@ class VoucherFundingMerchantController extends StorefrontController
             throw new \InvalidArgumentException('Please input voucher code to redeem');
         }
 
-        $merchant = SalesChannelContextExtension::extract($context);
-
         $data = $this->voucherFundingService->getVoucherStatus($voucherCode, $merchant, $context);
 
-        $status = $data['status'] == 'invalid' ? 400 : 200;
+        $status = $data['status'] === 'invalid' ? 400 : 200;
 
         return new JsonResponse(['data' => $data], $status);
     }
